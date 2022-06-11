cae204d5577f57a33b56b3df7fa6a5703fd5cb3f
GPV <104330277+GPVcode@users.noreply.github.com>
2022-06-11 10:04:12 -0700

    Create Welcome Site Function.js

---
+++ Welcome Site Function.js
@@ -0,0 +1,15 @@
+//Building a function to greet user approriately based on time of day. Example: "Good morning" if logged on at 10AM.
+let time;
+
+let userLoggedInToday = True;
+if (UserLoggedInToday && time === 12) {
+    
+    //insert action here
+    console.log("Good afternoon, thanks for logging in today!");
+    
+}
+else(){ //backup plan
+  
+  // insert action here
+  
+}
