bbff1db09f5b77b02cd1ab0865b8033d0bc4acaa
GPV <104330277+GPVcode@users.noreply.github.com>
2022-06-30 12:22:17 -0700

    Update strings-8.5.js

--- strings-8.5.js
+++ strings-8.5.js
@@ -23,11 +23,6 @@ undefined
 undefined
 > //we decide where to join
 undefined
-
-Uncaught SyntaxError: Invalid or unexpected token
-> titleArr.join("_-_";
-titleArr.join("_-_";
-              ^^^^^
 Uncaught SyntaxError: missing ) after argument list
 > titleArr.join("_-_");
 'The_-_Light_-_Fantastic'
@@ -58,3 +53,44 @@ Uncaught ReferenceError: author is not defined
 > //Solution: `The price of '${title}' by ${author} is $${price}.`;
 undefined
 >
+  
+// 8.5 ASSESSMWENT
+
+/*
+  Complete the function below according to the instructions.
+  
+  When the function's parameters reference `products`, they are referencing an array of objects. Each object has the following shape:
+   {
+     name: "Slip Dress",
+     priceInCents: 8800,
+     availableSizes: [ 0, 2, 4, 6, 10, 12, 16 ]
+   }
+*/
+
+function listAllItems(products) {
+
+  if(products.length === 0){
+    return `There are no items for sale.`  
+  }
+  
+  let items = [];
+  let itemsCount = 0;
+  
+  for(let i = 0; i < products.length; i++){ 
+    itemsCount++;
+    items.push(products[i].name);
+  }
+  if(items.length === 1){
+    return "There is 1 item for sale: " + items[0] + ".";
+  }
+  if(items.length === 2){
+    return "There are 2 items for sale: " + items.join(" and ") + ".";
+  }
+  if(items.length > 2){
+    return `There are ${itemsCount} items for sale: ` + items.join(", ") + ".";
+  }
+
+} //function closing bracket
+module.exports = {
+  listAllItems,
+};
